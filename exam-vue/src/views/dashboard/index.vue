d7dbf5945afc9797fb5cdebfeb9405d4927139a4
zgliuwudi@163.com <zgliuwudi@163.com>
2024-09-17 11:23:34 +0800

    commit

--- exam-vue/src/views/dashboard/index.vue
+++ exam-vue/src/views/dashboard/index.vue
@@ -4,16 +4,16 @@
       <el-col :span="16">
         <el-card class="box">
           <div slot="header" class="clearfix">
-            <span>项目信息</span>
-            <el-button style="float: right; padding: 3px 0; font-size: 12px" type="text">系统版本：{{ version }}</el-button>
+            <span>最新通知</span>
+            <!-- <el-button style="float: right; padding: 3px 0; font-size: 12px" type="text">系统版本：{{ version }}</el-button> -->
           </div>
 
           <div style="line-height: 32px;">
 
-            <div class="title" style="padding-top: 0px">系统介绍</div>
-            考试系统是一款基于SpringBoot+Vue开发的考试系统。包含在线考试、用户体系、错题训练、考试规则、智能算分等考试功能，流程通畅。
+            <div class="title" style="padding-top: 0px">2024年6月青少年等级考试安排</div>
+            <!-- 考试系统是一款基于SpringBoot+Vue开发的考试系统。包含在线考试、用户体系、错题训练、考试规则、智能算分等考试功能，流程通畅。 -->
 
-            <div class="title">技术栈</div>
+            <!-- <div class="title">技术栈</div>
             <div class="tags">
               <el-tag>SpringBoot</el-tag>
               <el-tag>Shiro</el-tag>
@@ -23,16 +23,37 @@
               <el-tag>Vue</el-tag>
               <el-tag>Vue2</el-tag>
               <el-tag>ElementUI</el-tag>
+            </div> -->
+
+            <div class="title">考试方式</div>
+            <div>
+              <div>1. 在线居家考试（全国）；</div>
+              <div>2. 对于符合线下考试要求的考试服务网点，经地方实地调研报学会总部批准后，可组织线下考试。</div>
             </div>
 
-            <div class="title">产品功能</div>
+            <div class="title">报名时间</div>
             <div>
-              <div>系统完善：完善的前后端分离架构、规范的接口文档</div>
-              <div>权限控制：基于Shiro和JWT开发的权限控制功能。</div>
-              <div>基础功能：系统配置、用户管理、部门管理、角色管理等。</div>
-              <div>题库管理：支持单选题、多选题、判断题、支持试题批量导入导出</div>
-              <div>考试权限：支持指定考试范围：完全公开、指定某些部门人员考试</div>
-              <div>在线考试：学员在线考试、查看分数、考试错题训练。</div>
+              <p>报名网址：www.qceit.org.cn<br/>
+                报名时间：2024年4月11日-6月12日16:00；<br/>
+                考生注册、登录后填报信息并完成缴费视为报名成功；<br/>
+                注意事项： <br/>
+                1.报名结束前，考生因个人原因可申请退考，全额退费。<br/>
+                2.报名结束后，因已启动考务组织程序、安排考试资源，不予退费。<br/>
+                3.未参加考试的，视为缺考，不予退费。
+              </p>
+            </div>
+
+            <div class="title">准考证下载</div>
+            <div>
+              <p>6月18日-考前1天<br/>
+              </p>
+            </div>
+
+            <div class="title">考前集中测试</div>
+            <div>
+              <p>6月20日19:00-21:00 软件编程（C语言）软件编程（Python）软件编程（图形化）<br/>
+                 6月27日19:00-21:00 无人机技术 三维创意设计 机器人技术<br/>
+              </p>
             </div>
 
           </div>
@@ -51,9 +72,9 @@
             <div class="title" style="padding-top: 0px">商业咨询</div>
             <div style="display: flex;">
               <div style="flex-grow: 1;">
-                <div>微信：gyh_yinzi（微信号）或搜索手机18603038204</div>
-                <div>手机：<a href="tel:18603038204">18603038204（郭女士）</a></div>
-                <div>邮箱：<a href="mailto:835487894@qq.com">835487894@qq.com</a> </div>
+                <div>微信号搜索手机13039111952</div>
+                <!-- <div>手机：<a href="tel:18603038204">18603038204（郭女士）</a></div> -->
+                <div>邮箱：<a href="mailto:13039111952@163.com">13039111952@163.com</a> </div>
               </div>
               <div style="width: 120px; display: flex; align-items: center; flex-direction: column; align-content: flex-end">
                 <img style="width: 100px; height: 100px" src="@/assets/contact.png" >
@@ -62,7 +83,7 @@
 
             </div>
 
-            <div class="title">支持网站</div>
+            <!-- <div class="title">支持网站</div>
             <div>
               <div>企业官网：<a href="https://www.yfhl.net" target="_blank">https://www.yfhl.net</a></div>
               <div>在线试用：<a href="https://exam.yfhl.net" target="_blank">https://exam.yfhl.net</a></div>
@@ -72,7 +93,7 @@
             <div style="display: flex; align-items: center">
               <img src="@/assets/h5.png" style="width: 150px; height: 150px; border: #efefef 1px solid">
               <img src="@/assets/mp.jpg" style="width: 150px; height: 150px; border: #efefef 1px solid; margin-left: 20px">
-            </div>
+            </div> -->
 
           </div>
 
@@ -112,6 +133,7 @@ export default {
   height: calc(100vh - 120px);
   font-size: 14px;
   line-height: 28px;
+  overflow-y: auto;
 }
 .title{
   font-size: 18px;
